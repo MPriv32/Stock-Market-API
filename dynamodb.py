7ccbe99ec8130c6ba7a24640f39b330a1d9ad377
Mitchell Privett <99053048+MPriv32@users.noreply.github.com>
2022-05-22 17:59:23 -0700

    Updated code for dbclient

--- dynamodb.py
+++ dynamodb.py
@@ -1,13 +1,21 @@
 import boto3
 import app
+import os
 
 #Pulling stock name and daily price from API
 stock_name = str(app.api_data.ticker)
 stock_price = str(app.api_data.daily_close)
 
+if os.path.isfile('.env'):
+    from dotenv import load_dotenv
+    load_dotenv()
+
+ACCESS_KEY = os.getenv('ACCESS_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 #DynamoDB table name
 table_name = "stock_tracker"
-dynamodb_client = boto3.client('dynamodb')
+dynamodb_client = boto3.client('dynamodb', region_name='us-west-2', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
 
 stock_info ={
     'Company': {'S': stock_name}
@@ -15,6 +23,8 @@ stock_info ={
 }
 
 print(stock_info)
+
+
 if __name__ == "__main__":
     
     dynamodb_client.put_item(TableName = table_name, Item = stock_info )
